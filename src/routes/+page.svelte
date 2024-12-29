fa98db5335b61771b691a5adfba5ac229007ca9b
andreecy <andreechristianyulianto@gmail.com>
2024-12-29 15:27:50 +0700

    export, tile set index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,22 @@
 	import { selectedColor } from '$lib/stores/pallete';
 	import { onMount } from 'svelte';
 
-	const w = 128;
-	const h = 128;
+	const originSize = 128;
 
 	let canvasOrigin: HTMLCanvasElement;
 	let canvasTiles: HTMLCanvasElement;
 	let canvasPaint: HTMLCanvasElement;
+
+	let tilesMousePos = $state({ x: 0, y: 0 });
+	let selectedTileIndex = $state(0);
 
 	let paintMousePos = $state({ x: 0, y: 0 });
 	let isMouseDown = $state(false);
 
 	// data of the original file
 	let tiles: Canvas;
+
+	let tileSize = $state(8);
 
 	async function updateCanvasOrigin() {
 		// for (let x = 0; x < w; x++) {
@@ -33,6 +37,21 @@
 		let originCtx = canvasOrigin.getContext('2d');
 		if (!originCtx) return;
 		originCtx.drawImage(tilesBitmap, 0, 0);
+	}
+
+	function exportPng() {
+		const dataURL = canvasOrigin.toDataURL('image/png');
+
+		// Create a temporary link element
+		const link = document.createElement('a');
+		link.setAttribute('download', 'tiles.png'); // Set the desired filename
+		link.setAttribute(
+			'href',
+			dataURL.replace(/^data:image\/png/i, 'data:application/octet-stream')
+		);
+
+		// Trigger a click event to initiate the download
+		link.click();
 	}
 
 	async function updateCanvasTiles() {
@@ -65,7 +84,7 @@
 		let paintCtx = canvasPaint.getContext('2d');
 		if (!paintCtx) return;
 		paintCtx.imageSmoothingEnabled = false;
-		paintCtx.drawImage(tilesBitmap, 0, 0, w, h);
+		paintCtx.drawImage(tilesBitmap, 0, 0, originSize, originSize);
 	}
 
 	function paintDraw() {
@@ -76,14 +95,32 @@
 	}
 
 	onMount(async () => {
-		tiles = new Canvas(w, h);
+		tiles = new Canvas(originSize, originSize);
 		updateCanvasOrigin();
 		updateCanvasTiles();
 		updateCanvasPaint();
 
+		canvasTiles.addEventListener('mousemove', function (event) {
+			const rect = canvasTiles.getBoundingClientRect();
+			const scale = 640 / originSize;
+			const x = Math.floor((event.clientX - rect.left) / scale);
+			const y = Math.floor((event.clientY - rect.top) / scale);
+			tilesMousePos = { x, y };
+		});
+
+		canvasTiles.addEventListener('mousedown', function () {
+			const { x, y } = tilesMousePos;
+			const col = Math.floor(x / tileSize);
+			const row = Math.floor(y / tileSize);
+			// get index of 16 rows and 16 cols tiles
+			const index = row * 16 + col;
+			console.log({ row, col, index });
+			selectedTileIndex = index;
+		});
+
 		canvasPaint.addEventListener('mousemove', function (event) {
 			const rect = canvasPaint.getBoundingClientRect();
-			const scale = 64;
+			const scale = 512 / tileSize;
 			const x = Math.floor((event.clientX - rect.left) / scale);
 			const y = Math.floor((event.clientY - rect.top) / scale);
 			paintMousePos = { x, y };
@@ -114,26 +151,27 @@
 
 <div class="flex h-screen items-center">
 	<div class="flex h-full flex-1 flex-row items-start gap-4 p-4">
-		<div class="flex-1">
+		<div class="flex flex-1 flex-col items-center gap-4">
 			<canvas
-				width={w}
-				height={h}
+				width={originSize}
+				height={originSize}
 				class="hidden border border-black"
 				style="width: 128px; height: 128px;"
 				bind:this={canvasOrigin}
 			></canvas>
 			<canvas
-				width={w}
-				height={h}
+				width={originSize}
+				height={originSize}
 				class="border border-black bg-black"
 				style="width: 640px; height: 640px;"
 				bind:this={canvasTiles}
 			></canvas>
+			<button class="bg-black px-4 py-2 text-white" onclick={exportPng}>Export</button>
 		</div>
 		<div class="flex flex-1 flex-col items-center gap-8">
 			<canvas
-				width={8}
-				height={8}
+				width={tileSize}
+				height={tileSize}
 				class="border border-black bg-black"
 				style="width: 512px; height: 512px;"
 				bind:this={canvasPaint}
